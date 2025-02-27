77cd96b29a2f7fad1460a30bdc2294a0ea21491d
prinson001 <prinsond53@gmail.com>
2025-02-27 17:14:04 +0530

    Modify auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,7 @@ const login = asyncHandler(async (req, res) => {
     throw new Error("email and password required");
   }
 
-  const user = await sql`select * from "User" where email =${req.body.email}`;
+  const user = await sql`select * from "users" where email =${req.body.email}`;
   console.log(user);
   if (user.length == 0) {
     res.status(400);
@@ -35,7 +35,7 @@ const updateUserData = async (req, res) => {
   const salt = await bcrypt.genSalt(10); // Generate salt with 10 rounds
   const hashedPassword = await bcrypt.hash("123", salt);
   const result = await sql`
-    UPDATE "User" 
+    UPDATE "users" 
     SET password = ${hashedPassword}
     WHERE id = 1
   `;
@@ -46,13 +46,25 @@ const updateUserData = async (req, res) => {
 };
 
 const getUsers = asyncHandler(async (req, res) => {
-  const users = await sql`select * from "User"`;
+  const users = await sql`select * from "users"`;
   if (users.length === 0) {
     res.status(200);
     throw new Error("No users in the database");
   }
   res.status(200);
   res.json({ users });
+});
+
+const createUser = asyncHandler(async (req, res) => {
+  const { email, firstname, familyname, password } = req.body;
+
+  if (!email || !firstname || !familyname || !password) {
+    res.status(400);
+    res.json({
+      status: "failure",
+      message: "email , firstname , familyname , password required",
+    });
+  }
 });
 
 module.exports = { login, updateUserData, getUsers };
