b251ef3c16486796276ed8f325c64bdc7a94efbb
prinson001 <prinsond53@gmail.com>
2025-04-06 21:18:48 +0530

    Resolve objective update modal : fields were not showing up

--- frontend/src/components/UpdateDynamicForm.jsx
+++ frontend/src/components/UpdateDynamicForm.jsx
@@ -311,6 +311,24 @@ const UpdateDynamicForm = ({
         className: "text-right",
       },
     ],
+    objective: [
+      {
+        name: "name",
+        label: "vendor name",
+        type: "text",
+        required: true,
+        columnSpan: 1,
+        className: "text-left",
+      },
+      {
+        name: "arabic_name",
+        label: "arabic name",
+        type: "text",
+        required: true,
+        columnSpan: 1,
+        className: "text-left",
+      },
+    ],
   });
 
   useEffect(() => {
